87674a7f5383373c23a40275fc739044f0e73138
MoYeRanqianzhi <108128735+MoYeRanqianzhi@users.noreply.github.com>
2022-12-04 11:58:11 +0800

    Add files via upload

---
+++ MobAI/answer/GS.py
@@ -0,0 +1,168 @@
+# GROUP SYSTEM
+"""
+GROUP SYSTEM IS THE ANSWER FOR MobAI
+"""
+
+ANSWER = {
+    '墨白连接测试': [['服务器连接正常!\n发信系统正常!\n语义分析系统正常!\n网络无异常!']],
+    '墨白官网': [['https://myr.budibase.app/app/moyeransoft#/moyeranmobai/mobai/main']],
+    'MobAI官网': [['https://myr.budibase.app/app/moyeransoft#/moyeranmobai/mobai/main']],
+    '机器人官网': [['https://myr.budibase.app/app/moyeransoft#/moyeranmobai/mobai/main']],
+    '官网': [['https://myr.budibase.app/app/moyeransoft#/moyeranmobai/mobai/main']],
+    '分享官网': [['[CQ:share,url=https://myr.budibase.app/app/moyeransoft#/moyeranmobai/mobai/main,title=MobAI]']],
+    '简单介绍': [['[CQ:tts,text=MobAI是由墨叶染千枝主导开发的一体式综合QQ机器人]']],
+    '墨白标识': [['[CQ:image,file=https://i.328888.xyz/img/2022/12/03/iqoxL.th.png]']],
+    '菜单'
+    :
+        [['''▁▁▁▁▁MobAI▁▁▁▁▁
+[CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63]
+▐[CQ:face,id=66]签到系统[CQ:face,id=66]‖[CQ:face,id=303]银行系统[CQ:face,id=302]▐
+▐[CQ:face,id=318]排行系统[CQ:face,id=318]‖[CQ:face,id=66]商城系统[CQ:face,id=66]▐
+▐[CQ:face,id=319]娱乐系统[CQ:face,id=319]‖[CQ:face,id=66]交易系统[CQ:face,id=66]▐
+▐[CQ:face,id=271]新闻系统[CQ:face,id=271]‖[CQ:face,id=66]订阅系统[CQ:face,id=66]▐
+▐[CQ:face,id=66]实用工具[CQ:face,id=66]‖[CQ:face,id=299]无限工具[CQ:face,id=299]▐
+▐[CQ:face,id=66]高级设置[CQ:face,id=66]‖[CQ:face,id=32]获得帮助[CQ:face,id=32]▐
+▐[CQ:face,id=66]群聊操作[CQ:face,id=66]‖[CQ:face,id=262]问题反馈[CQ:face,id=262]▐
+[CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63]''']]
+    ,
+    '签到系统'
+    :
+        [['''▁▁▁▁▁签到系统▁▁▁▁▁
+[CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63]
+[CQ:face,id=66]签到
+[CQ:face,id=66]个人面板
+[CQ:face,id=66]查看连签天数
+[CQ:face,id=66]查看累签天数
+[CQ:face,id=66]连签排行榜
+[CQ:face,id=66]累签排行榜
+[CQ:face,id=66]全球连签排行榜
+[CQ:face,id=66]全球累签排行榜
+[CQ:face,id=66]订阅签到提醒
+[CQ:face,id=66]关于签到
+[CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63]''']]
+    ,
+    '银行系统'
+    :
+        [['''▁▁▁▁▁银行系统▁▁▁▁▁
+[CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63]
+[CQ:face,id=66]签到
+[CQ:face,id=66]个人面板
+[CQ:face,id=66]借款系统
+[CQ:face,id=66]贷款系统
+[CQ:face,id=66]金币排行榜
+[CQ:face,id=66]财富排行榜
+[CQ:face,id=66]全球金币排行榜
+[CQ:face,id=66]全球财富排行榜
+[CQ:face,id=66]订阅银行早报
+[CQ:face,id=66]关于银行系统
+[CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63]''']]
+    ,
+    '排行系统'
+    :
+        [['''▁▁▁▁▁排行系统▁▁▁▁▁
+[CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63
+[CQ:face,id=66]金币排行榜
+[CQ:face,id=66]全球金币排行榜
+[CQ:face,id=66]财富排行榜
+[CQ:face,id=66]全球财富排行榜
+[CQ:face,id=66]房产排行榜
+[CQ:face,id=66]全球房产排行榜
+[CQ:face,id=66]连签排行榜
+[CQ:face,id=66]全球连签排行榜
+[CQ:face,id=66]累签排行榜
+[CQ:face,id=66]全球累签排行榜
+[CQ:face,id=66]奇珍异宝榜
+[CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63][CQ:face,id=63]''']]
+    ,
+    '商城系统'
+    :
+        [['''''']]
+    ,
+    '娱乐系统'
+    :
+        [['''''']]
+    ,
+    '交易系统'
+    :
+        [['''''']]
+    ,
+    '新闻系统'
+    :
+        [['''''']]
+    ,
+    '订阅系统'
+    :
+        [['''''']]
+    ,
+    '实用工具'
+    :
+        [['''''']]
+    ,
+    '无限工具'
+    :
+        [['''''']]
+    ,
+    '高级设置'
+    :
+        [['''''']]
+    ,
+    '获得帮助'
+    :
+        [['''''']]
+    ,
+    '群聊操作'
+    :
+        [['''''']]
+    ,
+    '问题反馈'
+    :
+        [['''''']]
+    ,
+    ''
+    :
+        [['''''']]
+    ,
+    '天官赐福': [['百无禁忌']],
+    '为你明灯三千': [['花开满城']],
+    '老将不死': [['薪火相传']],
+    '忘羡一曲终': [['曲终人不散']],
+    '东楼贺朝': [['西楼谢俞']],
+    '世界灿烂盛大': [['欢迎回家']],
+    '累了就抱抱空气': [['因为丞哥无处不在']],
+    '清华双杰': [['谋财害命']],
+    '长阶血未尽': [['那是他带你回家的路']],
+    '不知巫山客': [['不是命中人']],
+    '来时鲜衣怒马': [['去时仲永之伤']],
+    '我要赢一壶酒': [['拿来娶你']],
+    '顾飞': [['跟着光']],
+    '身在无间': [['心在桃源']],
+    '问灵十三载': [['等一不归人']],
+    '原耽语录': [['欢迎广大用户供稿原耽语录']],
+
+    '爱你孤身走暗巷': [['爱你不跪的模样']],
+    'come on的喂': [['ABC']],
+    '巴啦啦能量': [['小魔仙全身变']],
+    '鸡你太美': [['你干嘛啊~诶呦~'], ['你食不食油饼', '苏珊，不然紫砂']],
+
+    '原耽好书推荐': [
+        ['《天官赐福》'],
+        ['《全球高考》'],
+        ['《二哈和他的白猫师尊》'],
+        ['《杀破狼》'],
+        ['《犯罪心理》'],
+        ['《某某》'],
+        ['《破云》'],
+        ['《AWM[绝地求生]》'],
+        ['《POG[电竞]》'],
+        ['《将近酒》'],
+        ['《撒野》'],
+        ['《我喜欢你的信息素》'],
+        ['《这题超纲了》'],
+        ['《伪装学渣》'],
+        ['《两A相逢必有一O》'],
+        ['《乐可》'],
+        ['《姜可》'],
+    ],
+
+    'edwiiujbedwbweibwediedwib': '',
+}
