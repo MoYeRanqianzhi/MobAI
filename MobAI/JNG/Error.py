fb4be39ffe24ee86785ef0fceab480c07e230b54
YeRan Mo <108128735+MoYeRanqianzhi@users.noreply.github.com>
2022-12-18 00:37:42 +0800

    Delete MobAI/JNG directory

--- MobAI/JNG/Error.py
+++
@@ -1,9 +0,0 @@
-class MoYeRanError(Exception):
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        return repr(self.value)
-
-    __module__ = 'builtins'
-
